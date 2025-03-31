6f62e4371805d1a5a80ba053ee22ed4242dee65d
kirill <redtermenator@yandex.ru>
2025-04-01 01:38:45 +0300

    Initial commit

---
+++ Factorial/Factorial/Program.fs
@@ -0,0 +1,17 @@
+﻿let rec factorial x acc =
+    if x < 0 || x > 31 then // For numbers greater than 31, you need to use libraries for counting large numbers 
+        failwith "Incorrect number entered"
+    elif x <= 1 then 
+        acc
+    else 
+        factorial (x - 1) (acc * x)
+
+[<EntryPoint>]
+let main argv =
+    let x = 31
+    try
+        let result = factorial x 1
+        printfn "Result = %d" result
+    with
+    | ex -> printfn "Ошибка: %s" ex.Message
+    0
