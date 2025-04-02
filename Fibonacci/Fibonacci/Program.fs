a2f1210f86020a0b1ed815612af08d446f76bf82
kirill <redtermenator@yandex.ru>
2025-04-03 02:45:38 +0300

    Initial commit

---
+++ Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,13 @@
+﻿let rec fibonacci lastNum firstNum secondNum = 
+    if lastNum = 0 then
+        firstNum
+    elif lastNum = 1 then
+        secondNum
+    else
+        let nextNum = firstNum + secondNum
+        fibonacci 
+            (lastNum - 1)
+            secondNum
+            nextNum
+
+printfn $"{fibonacci 100 0 1}"
