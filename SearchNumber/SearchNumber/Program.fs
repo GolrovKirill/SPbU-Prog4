b5ee7188223e0ca0a662b04b588720c81d3d39d9
kirill <redtermenator@yandex.ru>
2025-04-03 19:29:17 +0300

    Initial commit

---
+++ SearchNumber/SearchNumber/Program.fs
@@ -0,0 +1,13 @@
+﻿let search list number =
+    let rec searchNumber list number i =
+        if List.isEmpty list then
+            None
+        elif number = List.head list then
+            Some i
+        else searchNumber (List.tail list) number (i + 1)
+       
+    searchNumber list number 0
+
+let list = [1; 2; 3; 4; 4]
+let result = search list 4
+printfn $"{result}"
