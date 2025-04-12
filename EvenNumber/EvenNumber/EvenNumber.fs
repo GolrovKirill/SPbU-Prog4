3e2de0ab3486704199567d93569b57cf44932344
kirill <redtermenator@yandex.ru>
2025-04-12 19:40:47 +0300

    Initial commit

---
+++ EvenNumber/EvenNumber/EvenNumber.fs
@@ -0,0 +1,16 @@
+﻿module EvenNumber
+    
+    // Count even number in list use filter.
+    let countEvenNumberWithFilter ls =
+        ls |> List.filter (fun x -> x % 2 = 0) |> List.length
+
+    // Count even number in list use map.
+    let countEvenNumberWithMap ls =
+        ls 
+          |> List.map (fun x -> if x % 2 = 0 then Some x else None)
+          |> List.choose id
+          |> List.length
+
+    // Count even number in list use fold.
+    let countEvenNumberWithFold ls =
+        ls |> List.fold(fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
---
+++ EvenNumber/EvenNumberTests/EvenNumberTests.fs
@@ -0,0 +1,37 @@
+﻿open NUnit.Framework
+open EvenNumber
+open FsUnit
+open FsCheck
+
+module EvenNumberTests = 
+  
+  [<Test>]
+  let ``Equal countEvenNumberWithFilter and countEvenNumberWithMap`` () =
+      let inputList = [1; 2; 3; 4; 5; 6]
+      let resultFilter = countEvenNumberWithFilter inputList
+      let resultMap = countEvenNumberWithMap inputList
+      resultFilter |> should equal resultMap
+
+  [<Test>]
+  let ``Equal countEvenNumberWithFilter and countEvenNumberWithFold`` () =
+      let inputList = [1; 2; 3; 4; 5; 6]
+      let resultFilter = countEvenNumberWithFilter inputList
+      let resultFold = countEvenNumberWithFold inputList
+      resultFilter |> should equal resultFold
+
+  [<Test>]
+  let ``Equal countEvenNumberWithFold and countEvenNumberWithMap`` () =
+      let inputList = [1; 2; 3; 4; 5; 6]
+      let resultFold = countEvenNumberWithFold inputList
+      let resultMap = countEvenNumberWithMap inputList
+      resultFold |> should equal resultMap
+
+  [<Test>]
+  let ``Test use FsCheck for countEvenNumberWithFilter and countEvenNumberWithMap and countEvenNumberWithFold`` () =
+    let test (ls: list<int>) = 
+        let resultFilter = countEvenNumberWithFilter ls
+        let resultFold = countEvenNumberWithFold ls
+        let resultMap = countEvenNumberWithMap ls
+
+        resultFilter = resultFold && resultFold = resultMap
+    Check.QuickThrowOnFailure test
