5301b3a5264009c46edea76509a6d1711d496368
kirill <redtermenator@yandex.ru>
2025-04-15 00:15:18 +0300

    Initial commit

---
+++ ParsingTree/ParsingTree/ParsingTree.fs
@@ -0,0 +1,31 @@
+﻿module ParsingTree
+
+type ParseTree =
+    | EmptyNode
+    | Number of value: float
+    | Symbol of symbol: string * left: ParseTree * right: ParseTree
+
+// Calculate the value of the parse tree.
+let calculateParseTree tree =
+    let rec calculateTree node =
+        match node with
+        | Number(value) -> value
+        | Symbol(symbol, left, right) ->
+            let leftValue = calculateTree left
+            let rightValue = calculateTree right
+        
+            match symbol with
+            | "+" -> leftValue + rightValue
+            | "-" -> leftValue - rightValue
+            | "*" -> leftValue * rightValue
+            | "/" -> 
+                if rightValue = 0.0 then failwith "Division by 0."
+                else leftValue / rightValue
+            | _ -> failwith "Entered incorrect operation."
+
+    match tree with
+    | EmptyNode -> failwith "Entered empty tree."
+    | _ ->
+        match tree with
+        | Number(_) -> failwith "Parse tree doesn't start with a number."
+        | Symbol(_, _, _) -> calculateTree tree
