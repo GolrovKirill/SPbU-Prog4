60949094284872752346e3d4be9434fee44095c8
kirill <redtermenator@yandex.ru>
2025-04-27 21:52:24 +0300

    Add new function and tests for them

--- ParsingTree/ParsingTree/ParsingTree.fs
+++ ParsingTree/ParsingTree/ParsingTree.fs
@@ -5,8 +5,67 @@ type ParseTree =
     | Number of value: float
     | Symbol of symbol: string * left: ParseTree * right: ParseTree
 
+// Check what entered parse tree.
+let checkTree tree =
+    match tree with
+    | EmptyNode -> failwith "Entered empty tree."
+    | Number(_) -> failwith "Parse tree doesn't start with a number."
+    | Symbol(_, _, _) -> true
+
+// Calculate the value of the parse tree use CPS with linearization.
+let calculateParseTreeCPS tree =
+    checkTree tree |> ignore
+    
+    let rec createList tree acc cont =
+        match tree with
+        | Symbol(sym, left, right) ->
+            createList left acc (fun leftAcc ->
+                createList right leftAcc (fun rightAcc ->
+                    cont (sym :: rightAcc)))
+        | Number(v) ->
+            cont (string(v) :: acc)
+
+    let listTree = List.rev(createList tree [] (fun x -> x))
+
+    let rec calculate (listTree: List<string>) (accNum: List<float>) cont =
+        
+        match listTree with
+        | head :: tail ->
+            match System.Double.TryParse(head) with
+            | (true, value) ->
+                calculate tail (value :: accNum) cont
+            | (false, _) when (List.contains head ["+"; "-"; "*"; "/"]) ->
+                if List.length accNum < 2 then
+                    failwithf "Not enough operands for operator '%s'." head
+                else
+                    let head1 = accNum.Tail.Head
+                    let head2 = accNum.Head
+                    match head with 
+                    | "+" -> 
+                        calculate tail ((head1 + head2) :: accNum.Tail.Tail) cont
+                    | "-" -> 
+                        calculate tail ((head1 - head2) :: accNum.Tail.Tail) cont
+                    | "*" -> 
+                        calculate tail ((head1 * head2) :: accNum.Tail.Tail) cont
+                    | "/" -> 
+                        if head2 = 0.0 then 
+                            failwith "Division by 0."
+                        else 
+                            calculate tail ((head1 / head2) :: accNum.Tail.Tail) cont
+                    | _ -> failwith "Entered incorrect operation."         
+            | _ -> failwith "Invalid token in expression."
+        | [] ->
+            match accNum with
+            | [result] -> cont result
+            | [] -> failwith "Empty expression."
+            | _ -> failwith "Expression could not be fully evaluated."
+
+    calculate listTree [] (fun x -> x)
+    
 // Calculate the value of the parse tree.
 let calculateParseTree tree =
+    checkTree tree |> ignore
+    
     let rec calculateTree node =
         match node with
         | Number(value) -> value
@@ -23,9 +82,4 @@ let calculateParseTree tree =
                 else leftValue / rightValue
             | _ -> failwith "Entered incorrect operation."
 
-    match tree with
-    | EmptyNode -> failwith "Entered empty tree."
-    | _ ->
-        match tree with
-        | Number(_) -> failwith "Parse tree doesn't start with a number."
-        | Symbol(_, _, _) -> calculateTree tree
+    calculateTree tree
--- ParsingTree/ParsingTreeTests/ParsingTreeTests.fs
+++ ParsingTree/ParsingTreeTests/ParsingTreeTests.fs
@@ -57,3 +57,57 @@ let ``calculateParseTree should calculate all operation in deep tree`` () =
     let tree = Symbol("+", Symbol("*", Symbol("-", Number(1.0), Number(2.0)), Number(3.0)), Symbol("/", Number(4.0), Number(5.0)))
     let result = calculateParseTree tree
     result |> should equal -2.2
+
+[<Test>]
+let ``calculateParseTreeCPS should fail with "Entered empty tree." for empty tree`` () =
+    let tree = EmptyNode
+    (fun () -> (calculateParseTreeCPS tree) |> ignore)
+    |> should throw typeof<System.Exception>
+
+[<Test>]
+let ``calculateParseTreeCPS should fail with "Parse tree doesn't start with a number." for a single number`` () =
+    let tree = Number(5.0)
+    (fun () -> (calculateParseTreeCPS tree) |> ignore)
+    |> should throw typeof<System.Exception>
+
+[<Test>]
+let ``calculateParseTreeCPS should fail with "Division by 0." when dividing by zero`` () =
+    let tree = Symbol("/", Number(20.0), Number(0.0))
+    (fun () -> (calculateParseTreeCPS tree) |> ignore)
+    |> should throw typeof<System.Exception>
+
+[<Test>]
+let ``calculateParseTreeCPS should fail with "Entered incorrect operation." for invalid operation`` () =
+    let tree = Symbol("invalid", Number(1.0), Number(2.0))
+    (fun () -> (calculateParseTreeCPS tree) |> ignore)
+    |> should throw typeof<System.Exception>
+
+[<Test>]
+let ``calculateParseTreeCPS should calculate addition correctly`` () =
+    let tree = Symbol("+", Number(3.0), Number(4.0))
+    let result = calculateParseTreeCPS tree
+    result |> should equal 7.0
+
+[<Test>]
+let ``calculateParseTreeCPS should calculate subtraction correctly`` () =
+    let tree = Symbol("-", Number(10.0), Number(4.0))
+    let result = calculateParseTreeCPS tree
+    result |> should equal 6.0
+
+[<Test>]
+let ``calculateParseTreeCPS should calculate multiplication correctly`` () =
+    let tree = Symbol("*", Number(2.0), Number(5.0))
+    let result = calculateParseTreeCPS tree
+    result |> should equal 10.0
+
+[<Test>]
+let ``calculateParseTreeCPS should calculate division correctly`` () =
+    let tree = Symbol("/", Number(20.0), Number(5.0))
+    let result = calculateParseTreeCPS tree
+    result |> should equal 4.0
+
+[<Test>]
+let ``calculateParseTreeCPS should calculate all operation in deep tree`` () =
+    let tree = Symbol("+", Symbol("*", Symbol("-", Number(1.0), Number(2.0)), Number(3.0)), Symbol("/", Number(4.0), Number(5.0)))
+    let result = calculateParseTreeCPS tree
+    result |> should equal -2.2
