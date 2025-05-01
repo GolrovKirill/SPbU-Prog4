f134a6f7955e034d3d527342556fd028b4ddcd82
kirill <redtermenator@yandex.ru>
2025-05-02 00:58:05 +0300

    Removed unnecessary files, made a factorial wrapper function, replaced exceptions with None

--- Factorial/Factorial/Program.fs
+++ Factorial/Factorial/Program.fs
@@ -1,18 +1,26 @@
-﻿let rec factorial x acc =
-    if x < 0.0 || x > 31.0 || (x % 1.0 <> 0.0) then // For numbers greater than 31, you need to use libraries for counting large numbers 
-        failwith "Incorrect number entered"
-    elif x <= 1.0 then 
-        acc
-    else 
-        factorial (x - 1.0) (acc * x)
+﻿/// The function returns the factorial of an integer x
+let rec factorial x =
+    let rec calculate x acc = 
+        match x with
+        | Some x ->
+            match x with
+            | x when (x < 0.0 || x > 31.0 || (x <> floor x)) -> None
+            | x when (x <= 0.0) -> (Some acc)
+            | _ -> calculate (Some(x - 1.0)) (acc * x)
+        | None -> None
+    
+    calculate x 1.0
+        
 
 [<EntryPoint>]
 let main argv =
-    let x = -2.765
+    let x = 5
     let num = float x
     try
-        let result = factorial num 1.0
-        printfn $"Result = {result}"
+        let result = factorial (Some num)
+        match result with
+        | Some result -> printfn $"Result = {result}"
+        | None -> printfn "Result = None"       
     with
     | ex -> printfn "Ошибка: %s" ex.Message
     0
