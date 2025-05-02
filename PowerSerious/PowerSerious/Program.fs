e4a62b9494551ad904ed48070899b690b6145a12
kirill <redtermenator@yandex.ru>
2025-05-02 14:22:21 +0300

    Add pattern match and add check for input parametrs

--- PowerSerious/PowerSerious/Program.fs
+++ PowerSerious/PowerSerious/Program.fs
@@ -1,25 +1,36 @@
-﻿let revers list = 
-   if List.isEmpty list then
-        []
-    else
+﻿/// A function that reverses an input list of integers.
+let revers list = 
+    match list with
+    | [] -> []
+    | _ ->
         let rec reversList firstList secondList =
-            if List.isEmpty firstList then
-                secondList
-            else reversList (List.tail firstList) ((List.head firstList) :: secondList)
+            match firstList with
+            | [] -> secondList
+            | _ ->
+                reversList (List.tail firstList) ((List.head firstList) :: secondList)
 
         reversList list []
 
+/// A function that constructs a series of powers of two from the first given parameter to the second.
 let listPowerNumber n m =
     let rec listPower i m num list =
-        if i = m then
+        match i with
+        | i when i = m ->
             (num * 2) :: list
-        else listPower (i + 1) m (num * 2) (num :: list)
+        | _ ->
+            listPower (i + 1) m (num * 2) (num :: list)
 
-    let num = 1 <<< n
-    let listResult = listPower 0 m num []
-    let result = revers listResult
-    result
+    match (n, m) with
+    |(n, m) when n <= m ->
+        let num = 1 <<< n
+        let listResult = listPower 0 m num []
+        revers listResult
+    | _ -> 
+        []
 
 let printList = listPowerNumber 1 3
-for element in printList do
-    printf $"{element} "
+match printList with
+| [] -> printf "Empty list"
+| _ -> 
+    for element in printList do
+        printf $"{element} "
