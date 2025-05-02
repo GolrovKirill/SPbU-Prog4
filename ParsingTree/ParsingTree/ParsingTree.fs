af7d332adcd2a58e0d86a53f6ac062a6f29618cd
kirill <redtermenator@yandex.ru>
2025-05-03 00:27:26 +0300

    Added updated calculateParseTreeCPS function and corrected comments

--- ParsingTree/ParsingTree/ParsingTree.fs
+++ ParsingTree/ParsingTree/ParsingTree.fs
@@ -5,81 +5,60 @@ type ParseTree =
     | Number of value: float
     | Symbol of symbol: string * left: ParseTree * right: ParseTree
 
-// Check what entered parse tree.
+/// Function that checks the input parse tree.
 let checkTree tree =
     match tree with
-    | EmptyNode -> failwith "Entered empty tree."
-    | Number(_) -> failwith "Parse tree doesn't start with a number."
+    | EmptyNode -> false
+    | Number(_) -> false
     | Symbol(_, _, _) -> true
 
-// Calculate the value of the parse tree use CPS with linearization.
+/// Function that calculates the input parse tree using CPS.
 let calculateParseTreeCPS tree =
-    checkTree tree |> ignore
-    
-    let rec createList tree acc cont =
-        match tree with
-        | Symbol(sym, left, right) ->
-            createList left acc (fun leftAcc ->
-                createList right leftAcc (fun rightAcc ->
-                    cont (sym :: rightAcc)))
-        | Number(v) ->
-            cont (string(v) :: acc)
+    if not (checkTree tree) then None else
 
-    let listTree = List.rev(createList tree [] (fun x -> x))
+    let rec calculateTreeCPS node cont =
+        match node with
+        | EmptyNode -> cont None
+        | Number value -> cont (Some value)
+        | Symbol(symbol, left, right) ->
+            calculateTreeCPS left (fun leftValueOption ->
+                match leftValueOption with
+                | None -> cont None
+                | Some leftValue ->
+                    calculateTreeCPS right (fun rightValueOption ->
+                        match rightValueOption with
+                        | None -> cont None
+                        | Some rightValue ->
+                            match symbol with
+                            | "+" -> cont (Some (leftValue + rightValue))
+                            | "-" -> cont (Some (leftValue - rightValue))
+                            | "*" -> cont (Some (leftValue * rightValue))
+                            | "/" ->
+                                if rightValue = 0.0 then cont None
+                                else cont (Some (leftValue / rightValue))
+                            | _ -> cont None))
 
-    let rec calculate (listTree: List<string>) (accNum: List<float>) cont =
-        
-        match listTree with
-        | head :: tail ->
-            match System.Double.TryParse(head) with
-            | (true, value) ->
-                calculate tail (value :: accNum) cont
-            | (false, _) when (List.contains head ["+"; "-"; "*"; "/"]) ->
-                if List.length accNum < 2 then
-                    failwithf "Not enough operands for operator '%s'." head
-                else
-                    let head1 = accNum.Tail.Head
-                    let head2 = accNum.Head
-                    match head with 
-                    | "+" -> 
-                        calculate tail ((head1 + head2) :: accNum.Tail.Tail) cont
-                    | "-" -> 
-                        calculate tail ((head1 - head2) :: accNum.Tail.Tail) cont
-                    | "*" -> 
-                        calculate tail ((head1 * head2) :: accNum.Tail.Tail) cont
-                    | "/" -> 
-                        if head2 = 0.0 then 
-                            failwith "Division by 0."
-                        else 
-                            calculate tail ((head1 / head2) :: accNum.Tail.Tail) cont
-                    | _ -> failwith "Entered incorrect operation."         
-            | _ -> failwith "Invalid token in expression."
-        | [] ->
-            match accNum with
-            | [result] -> cont result
-            | [] -> failwith "Empty expression."
-            | _ -> failwith "Expression could not be fully evaluated."
+    calculateTreeCPS tree id
 
-    calculate listTree [] (fun x -> x)
-    
-// Calculate the value of the parse tree.
+/// Function that calculates the input parse tree.
 let calculateParseTree tree =
-    checkTree tree |> ignore
+    if not (checkTree tree) then None else
     
     let rec calculateTree node =
         match node with
-        | Number(value) -> value
+        | EmptyNode -> None
+        | Number(value) -> Some value
         | Symbol(symbol, left, right) ->
             let leftValue = calculateTree left
             let rightValue = calculateTree right
         
-            match symbol with
-            | "+" -> leftValue + rightValue
-            | "-" -> leftValue - rightValue
-            | "*" -> leftValue * rightValue
-            | "/" -> 
-                if rightValue = 0.0 then failwith "Division by 0."
-                else leftValue / rightValue
-            | _ -> failwith "Entered incorrect operation."
+            match (symbol, leftValue, rightValue) with
+            | ("+", Some leftValue, Some rightValue) -> Some (leftValue + rightValue)
+            | ("-", Some leftValue, Some rightValue) -> Some (leftValue - rightValue)
+            | ("*", Some leftValue, Some rightValue) -> Some (leftValue * rightValue)
+            | ("/", Some leftValue, Some rightValue) -> 
+                if rightValue = 0.0 then None
+                else Some (leftValue / rightValue)
+            | _ -> None
 
     calculateTree tree
