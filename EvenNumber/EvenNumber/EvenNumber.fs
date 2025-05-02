9f375eaa1beeb69cb26b441cee6d8c7f1cfff907
kirill <redtermenator@yandex.ru>
2025-05-02 18:09:55 +0300

    Removed unnecessary files and added GenerateProgramFile

--- EvenNumber/EvenNumber/EvenNumber.fs
+++ EvenNumber/EvenNumber/EvenNumber.fs
@@ -1,16 +1,16 @@
 ﻿module EvenNumber
     
-    // Count even number in list use filter.
+    /// The function to count even numbers in a list uses a filter.
     let countEvenNumberWithFilter ls =
         ls |> List.filter (fun x -> x % 2 = 0) |> List.length
 
-    // Count even number in list use map.
+    /// The function to count even numbers in a list uses a map.
     let countEvenNumberWithMap ls =
         ls 
-          |> List.map (fun x -> if x % 2 = 0 then Some x else None)
-          |> List.choose id
-          |> List.length
+            |> List.map (fun x -> if x % 2 = 0 then Some x else None)
+            |> List.choose id
+            |> List.length
 
-    // Count even number in list use fold.
+    /// The function to count even numbers in a list uses a fold.
     let countEvenNumberWithFold ls =
-        ls |> List.fold(fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
+        ls |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
