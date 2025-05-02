0fc1e1bedad2d92789a19a16d5e06d329794ee18
kirill <redtermenator@yandex.ru>
2025-05-02 03:09:02 +0300

    Add pattern match

--- SearchNumber/SearchNumber/Program.fs
+++ SearchNumber/SearchNumber/Program.fs
@@ -1,13 +1,17 @@
-﻿let search list number =
+﻿/// A function that searches for the position of a number in a passed list of integers.
+let search list number =
     let rec searchNumber list number i =
-        if List.isEmpty list then
-            None
-        elif number = List.head list then
-            Some i
-        else searchNumber (List.tail list) number (i + 1)
-       
+        match list with
+        | [] -> None
+        | head::tail -> 
+            match head with
+            | head when head = number -> Some i
+            | _ ->  searchNumber tail number (i + 1)
+
     searchNumber list number 0
 
 let list = [1; 2; 3; 4; 4]
 let result = search list 4
-printfn $"{result}"
+match result with
+| Some result -> printfn $"{result}"
+| None -> printfn "Absent"
