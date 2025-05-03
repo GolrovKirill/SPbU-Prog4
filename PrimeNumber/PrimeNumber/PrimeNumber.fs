db963f4d7fd94bf5f906cbbc3e966252aad0c6b5
kirill <redtermenator@yandex.ru>
2025-05-03 14:29:15 +0300

    Add function A function that generates a sequence of prime numbers

--- PrimeNumber/PrimeNumber/PrimeNumber.fs
+++ PrimeNumber/PrimeNumber/PrimeNumber.fs
@@ -1,14 +1,18 @@
 ﻿module PrimeNumber
-    /// Function that checks the primality of a number.
-    let selectPrimeNumber x =
-        match x with
-        | x when x < 2 -> false
-        | _ -> 
-            let rec select i =
-                match i with
-                | i when i * i > x -> true
-                | _ -> 
-                    match x with
-                    | x when x % i = 0 -> false
-                    | _ -> select (i + 1)
-            select 2
+
+/// Function that checks the primality of a number.
+let selectPrimeNumber x =
+    match x with
+    | x when x < 2 -> false
+    | _ -> 
+        let rec select i =
+            match i with
+            | i when i * i > x -> true
+            | _ -> 
+                match x with
+                | x when x % i = 0 -> false
+                | _ -> select (i + 1)
+        select 2
+
+/// A function that generates a sequence of prime numbers.
+let sequencePrime = Seq.initInfinite (fun x -> x + 2) |> Seq.filter selectPrimeNumber
--- PrimeNumber/PrimeTests/PrimeNumberTest.fs
+++ PrimeNumber/PrimeTests/PrimeNumberTest.fs
@@ -1,29 +1,33 @@
-﻿open PrimeNumber
+﻿module PrimeTests
+
+open PrimeNumber
 open NUnit.Framework
 open FsUnit
-
-
-module PrimeTests =  
     
-    [<Test>]
-    let ``2 is a prime number`` () =
-        selectPrimeNumber 2 |> should equal true
+[<Test>]
+let ``2 is a prime number`` () =
+    selectPrimeNumber 2 |> should equal true
 
-    [<Test>]
-    let ``3 is a prime number`` () =
-        selectPrimeNumber 3 |> should equal true
+[<Test>]
+let ``3 is a prime number`` () =
+    selectPrimeNumber 3 |> should equal true
 
-    [<Test>]
-    let ``4 is not a prime number`` () =
-        selectPrimeNumber 4 |> should equal false
+[<Test>]
+let ``4 is not a prime number`` () =
+    selectPrimeNumber 4 |> should equal false
 
-    [<Test>]
-    let ``5 is a prime number`` () =
-        selectPrimeNumber 5 |> should equal true
+[<Test>]
+let ``5 is a prime number`` () =
+    selectPrimeNumber 5 |> should equal true
 
-    [<Test>]
-    let ``Check first few primes`` () =
-        let expectedPrimes = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29]
-        let actualPrimes = Seq.initInfinite (fun x -> x + 2) |> Seq.filter selectPrimeNumber |> Seq.take 10 |> Seq.toList
+[<Test>]
+let ``Check first few primes`` () =
+    let expectedPrimes = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29]
+    let actualPrimes = sequencePrime |> Seq.take 10 |> Seq.toList
 
-        actualPrimes |> should equal expectedPrimes
+    actualPrimes |> should equal expectedPrimes
+
+[<EntryPoint>]
+let main argv =
+    0
+
