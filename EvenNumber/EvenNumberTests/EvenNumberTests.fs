e2f547389fb0ed5bcf4377f5be191fa6400b7644
kirill <redtermenator@yandex.ru>
2025-05-03 13:48:50 +0300

    Fix warning FS0988

--- EvenNumber/EvenNumberTests/EvenNumberTests.fs
+++ EvenNumber/EvenNumberTests/EvenNumberTests.fs
@@ -1,33 +1,33 @@
-﻿open NUnit.Framework
+﻿module EvenNumberTests
+
+open NUnit.Framework
 open EvenNumber
 open FsUnit
 open FsCheck
 
-module EvenNumberTests = 
-  
-  [<Test>]
-  let ``Equal countEvenNumberWithFilter and countEvenNumberWithMap`` () =
-      let inputList = [1; 2; 3; 4; 5; 6]
-      let resultFilter = countEvenNumberWithFilter inputList
-      let resultMap = countEvenNumberWithMap inputList
-      resultFilter |> should equal resultMap
+[<Test>]
+let ``Equal countEvenNumberWithFilter and countEvenNumberWithMap`` () =
+    let inputList = [1; 2; 3; 4; 5; 6]
+    let resultFilter = countEvenNumberWithFilter inputList
+    let resultMap = countEvenNumberWithMap inputList
+    resultFilter |> should equal resultMap
 
-  [<Test>]
-  let ``Equal countEvenNumberWithFilter and countEvenNumberWithFold`` () =
-      let inputList = [1; 2; 3; 4; 5; 6]
-      let resultFilter = countEvenNumberWithFilter inputList
-      let resultFold = countEvenNumberWithFold inputList
-      resultFilter |> should equal resultFold
+[<Test>]
+let ``Equal countEvenNumberWithFilter and countEvenNumberWithFold`` () =
+    let inputList = [1; 2; 3; 4; 5; 6]
+    let resultFilter = countEvenNumberWithFilter inputList
+    let resultFold = countEvenNumberWithFold inputList
+    resultFilter |> should equal resultFold
 
-  [<Test>]
-  let ``Equal countEvenNumberWithFold and countEvenNumberWithMap`` () =
-      let inputList = [1; 2; 3; 4; 5; 6]
-      let resultFold = countEvenNumberWithFold inputList
-      let resultMap = countEvenNumberWithMap inputList
-      resultFold |> should equal resultMap
+[<Test>]
+let ``Equal countEvenNumberWithFold and countEvenNumberWithMap`` () =
+    let inputList = [1; 2; 3; 4; 5; 6]
+    let resultFold = countEvenNumberWithFold inputList
+    let resultMap = countEvenNumberWithMap inputList
+    resultFold |> should equal resultMap
 
-  [<Test>]
-  let ``Test use FsCheck for countEvenNumberWithFilter and countEvenNumberWithMap and countEvenNumberWithFold`` () =
+[<Test>]
+let ``Test use FsCheck for countEvenNumberWithFilter and countEvenNumberWithMap and countEvenNumberWithFold`` () =
     let test (ls: list<int>) = 
         let resultFilter = countEvenNumberWithFilter ls
         let resultFold = countEvenNumberWithFold ls
@@ -35,3 +35,7 @@ module EvenNumberTests =
 
         resultFilter = resultFold && resultFold = resultMap
     Check.QuickThrowOnFailure test
+
+[<EntryPoint>]
+let main argv =
+    0
